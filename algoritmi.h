7bbb8757ef71301af53f2a8b81d7ebf4a7ac45a3
AldeaDragos <dragosaldea14@gmail.com>
2024-06-03 19:14:17 +0300

    no message

--- algoritmi.h
+++ algoritmi.h
@@ -221,11 +221,50 @@ void secvCresc(int v[], int dim) {
 
 }
 
-bool kSecventa(int v[], int dim,int k) {
 
 
-	if (dim % k != 0) {
-		return false;
+void nrSecvente(int v[], int dim, int k, int t) {
+
+	int ct = 0;
+	for (int i = 0; i < dim; i++) {
+		bool valid = true;
+		for (int j =i; j < k; j++) {
+			if (v[j] > t) {
+				valid = false;
+			}
+		}
+		if (valid == true) {
+			ct++;
+		}
+	}
+	cout << "Numarul de secvente de lungime " << k << " cu valori mai mici sau egale cu " << t << " este: " << ct << endl;
+}
+
+void vectorFrecventaNumere(int v[], int dim, int f[]) {
+	for (int i = 0; i < dim; i++) {
+		f[v[i]]++;
+	}
+}
+
+
+int douaElemEgale(int v[], int dim) {
+
+	for (int i = 0; i < dim; i++) {
+		for (int j = i + 1; j < dim; j++) {
+			if (v[i] == v[j]) {
+				return true;
+			}
+		}
+
+	}
+	return false;
+}
+
+
+void afisareVec(int v[], int dim) {
+
+	for (int i = 0; i < dim; i++) {
+		cout << v[i] << " ";
 	}
 
 }
@@ -233,63 +272,57 @@ bool kSecventa(int v[], int dim,int k) {
 
 void kSecventa1(int v[], int dim, int k) {
 
-	int ct = 0;
+	int f[100]{};
+	for (int i = 0; i < dim; i++) {
+		int j = i + 1;
+		int ct = 0;
+		while (j < dim && v[i] == v[j]) {
 
-	for (int i = 0; i < dim - k; i++) {
-
-		for (int j = i + 1; j < dim - k; j++) {
-
-			if (v[i] == v[j]);
-
+			ct++;
+			j++;
 		}
+		f[v[i]] = ct;
+		i = j;
 
+		
 	}
+
+	if (douaElemEgale(f, dim) == true) {
+		cout << "exista" << endl;
+	}
+	else {
+		cout << "nu exista" << endl;
+	}
+
 }
 
-
-void nrSecvente(int v[], int dim, int t, int k) {
-
-	int smax = 0;
-	int dmax = 0;
-
-	int ct = 0;
-
-	for (int i = 0; i < dim - t; i++) {
-
-		for (int j = i; j < dim; j++) {
-
-			if (v[j] <= t) {
-
-				if (j - i > dmax - smax) {
-					smax = i;
-					dmax = j;
-					ct++;
-				}
-			}
-		}
-	}
-	cout << "Numarul de secvente de lungime " << k << " cu valori mai mici sau egale cu " << t << " este: " << ct << endl;
-}
 
 void SecvEgale1(int v[], int dim) {
 
 	int smax = 0;
 	int dmax = 0;
+	int l = 0;
+	int maxl = 0;
 
-	for (int i = 0; i < dim; i++) {
-		for (int j = i + 1; j < dim; j++) {
+	for (int i =0; i <= dim; i++) {
+		for (int j = dim - 1; j > i; j--) {
 			if (v[i] == v[j]) {
-
-
+				l = j - i + 1;
+			}
+			if (l > maxl) {
+				smax = i;
+				dmax = j;
+				maxl = l;
 			}
 		}
 	}
+	cout << smax << " " << dmax;
 }
 
 
 void SecvEgale2(int v[], int dim) {
 
-	int ct = 0;
+	int ct = 0; 
 
 
 	for (int i = 0; i < dim; i++) {
@@ -304,4 +337,148 @@ void SecvEgale2(int v[], int dim) {
 			i = j;
 		}
 	cout << "Numarul de secvente cu elemente egale este: " << ct << endl;
+}
+
+void Secvente1(int v[], int dim, int k) {
+
+	int ct = 1;
+	int nrElemImp = 0;
+
+	for (int i = 0; i < dim; i++) {
+		if (v[i] % 2 != 0) {
+			nrElemImp++;
+		}
+		if (nrElemImp > k) {
+			ct++;
+		}
+	}
+	cout << ct << endl;
+}
+
+
+int pozPrimElemPare(int v[], int dim) {
+
+	for (int i = 0; i < dim; i++) {
+		if (v[i] % 2 == 0) {
+			return i;
+		}
+	}
+}
+
+int pozUltimElemPare(int v[], int dim) {
+
+	for (int i = dim-1; i >= 0; i--) {
+		if (v[i] % 2 == 0) {
+			return i;
+		}
+	}
+}
+
+
+int secventaVectoParSuma(int v[], int dim) {
+
+	int primulElem = pozPrimElemPare(v, dim);
+	int ultimulElem = pozUltimElemPare(v, dim);
+	int suma = 0;
+
+	for (int i = primulElem; i <= ultimulElem; i++) {
+		suma += v[i];
+	}
+
+	return suma;
+}
+
+void sumaSecvMax(int v[], int dim) {
+
+	int suma = secventaVectoParSuma(v, dim);
+	
+	cout << " Suma este " << suma << endl;
+
+	bool gasit = true;
+	for (int i = 0; i < dim; i++) {
+		int sum = 0;
+		for (int j = i; j < dim; j++) {
+			sum += v[j];
+			if (sum<suma) {
+				gasit = true;
+				for (int k = i; k <= j; k++) {
+					cout << v[k] << " ";
+				}
+				cout << endl;
+			}
+		}
+	}
+}
+
+void secvCresc1(int v[], int dim) {
+	int smax = 0;
+	int dmax = 0;
+	int ct = 0;
+
+	for (int i = 0; i < dim; i++) {
+		int j = i;
+		while (j < dim - 1 && v[j] <= v[j + 1]) {
+			j++;
+		}
+		if (j - i + 1 > dmax - smax) {
+			smax = i;
+			dmax = j;
+		}
+		i = j;
+
+	}
+	cout << smax+1 << " " << dmax+1;
+
+}
+
+void secv10(int v[], int dim) {
+
+	int ct = 0;
+	int lungimeMaxima = 0;
+
+	for (int i = 0; i < dim; i++) {
+
+		for (int j = i; j < dim; j++) {
+			if (v[j] % 10 == 0) {
+				ct++;
+				lungimeMaxima = j - i + 1;
+			}
+		}
+
+	}
+	cout << " Numarul de secventa cu elemente divizibile cu 10 este " << ct << " iar lungimea maxima este " << lungimeMaxima << endl;
+}
+
+void SumaInSecv(int v[], int dim, int S) {
+
+	for (int i = 0; i < dim; i++) {
+		int sum = 0;
+		for (int j = i; j < dim; j++) {
+			sum += v[j];
+			if (sum == S) {
+				for (int k = i; k <= j; k++) {
+					cout << v[k] << " ";
+				}
+			}
+		}
+	}
+}
+
+void Memory006(int v[], int dim, int k) {
+
+	int putere = 1 << k;
+
+	int ct = 0;
+
+	for (int i = 0; i < dim; i++) {
+		int produs = 1;
+		for (int j = i; j < dim; j++) {
+			produs *= v[j];
+			if (produs == putere) {
+				ct++;
+
+			}
+		}
+	}
+	cout << ct << endl;
 }
--- solutii.h
+++ solutii.h
@@ -57,7 +57,7 @@ void sol4() {
 
 //Se dă un vector x cu n elemente, numere naturale și un vector y cu m elemente, numere naturale. Să se determine de câte ori este vectorul y secvență în vectorul x.
 
-void tm1() {
+void sol5() {
 
 	int date1[] = { 8,5,8,5,8,3,8,5,8,6 };
 
@@ -72,7 +72,7 @@ void tm1() {
 }
 
 //Se dă un şir format din n elemente, numere naturale. Calculaţi suma elementelor din secvenţa determinată de primul şi ultimul element prim.
-void tm2() {
+void sol6() {
 
 	int date[] = { 12,10,15,7,17,10,19,14 };
 
@@ -83,7 +83,7 @@ void tm2() {
 }
 
 //Se dă un vector format din n elemente, numere naturale nenule, şi un număr natural S. Determinaţi, dacă există o secvenţă de elemente din şir cu suma elementelor egală cu S.
-void tm3() {
+void sol7() {
 
 	int date[] = { 12,10,15,7,17,13,19,14 };
 
@@ -96,7 +96,7 @@ void tm3() {
 }
 
 //Se dă un şir format din n elemente, numere naturale.Determinaţi câte secvenţe maximale cu elementele ordonate crescător conţine şirul.
-void tm4() {
+void sol8() {
 
 	int date[] = { 12,10,15,17,17,10,12,14 };
 
@@ -106,21 +106,22 @@ void tm4() {
 
 }
 
-//Se dă un vector cu n elemente, numere naturale, și un număr k, divizor al lui n. Se împarte vectorul în k secvențe disjuncte, numerotate de la 1 la k. Să se stabilească dacă există două secvențe identice.
-void tm5() {
+//Se dă un vector cu n elemente, numere naturale și două numere t și k. Să se determine câte secvențe din șir au lungimea k și sunt formate din valori mai mici sau egale cu t.
+void sol9() {
 
-	int date[] = { 3,6,4,7,6,6,6,4,6,7,6,6,4,4,5 };
+	int date[] = { 1,4,3,5,7,3,4,2 };
 
-	int dim = 15;
+	int dim = 3;
 
-	int k = 5;
+	int k = 3;
 
+	int t = 5;
 
-
+	nrSecvente(date, dim, k, t);
 }
 
 //Se dă un vector cu n elemente, numere naturale, și un număr k. Să se stabilească dacă în vector există două secvențe de lungime k identice.
-void tm6() {
+void sol10() {
 
 	int date[] = { 2,3,1,1,4,3,1,1,4,3,8,8 };
 
@@ -132,40 +133,118 @@ void tm6() {
 
 }
 
-//Se dă un vector cu n elemente, numere naturale și două numere t și k. Să se determine câte secvențe din șir au lungimea k și sunt formate din valori mai mici sau egale cu t.
-
-void tm7() {
-
-	int date[] = { 1,4,3,5,7,3,4,2 };
-
-	int dim = 8;
-
-	int t = 5;
-
-	int k = 3;
-
-	nrSecvente(date, dim, t, k);
-
-}
-
+//TEMA
+//
+//
 //Se dă un vector cu n elemente, numere naturale. Determinați cea mai lungă secvență de elemente din vector care începe și se termină cu aceeași valoare. Dacă în vector există mai multe secvențe corecte de lungime maximă se va determina cea mai din stânga.
-void tm8() {
+void sol11() {
 
 	int date[] = { 6,6,8,3,6,6,3,8,4,3,3,4 };
 
 	int dim = 12;
 
-
+	SecvEgale1(date, dim);
 
 }
 
 //Se dă un vector cu n elemente, numere naturale. Determinați câte secvențe ale vectorului au toate elementele egale.
-void tm9() {
+void sol12() {
 
-	int date[] = { 4,3,3,5 };
+	int date[] = { 4,3,3,3,5 };
+
+	int dim = 5;
+
+	SecvEgale2(date, dim);
+
+}
+
+void sol13() {
+
+	int date[] = { 4,3,4,9,9,1,8 };
+
+	int dim = 7;
+
+	int k = 2;
+
+	Secvente1(date, dim, k);
+}
+
+void sol14() {
+
+	int date[] = { 12,10,15,7,17,10,19,14 };
 
 	int dim = 8;
 
-	SecvEgale2(date, dim);
+	secventaVectorSumaPrim(date, dim);
+}
+
+//Se numește secvență într-un vector toate elementele din acel vector cu indici cuprinși între două valori date.
+void sol15() {
+
+	int date[] = { 2,6,4,5,8,8,6,3,4 };
+
+	int dim = 9;
+
+	sumaSecvMax(date, dim);
+
+}
+
+//Se numește secvență într - un vector toate elementele din acel vector cu indici cuprinși între două valori date.
+void sol16() {
+
+	int date[] = { 2,6,4,5,8,9,6,3,4 };
+
+	int dim = 9;
+
+	secvCresc1(date, dim);
+
+}
+
+//Se dau n numere naturale, reprezentând elementele unui vector. Determinați lungimea maximă a unei secvențe de elemente divizibile cu 10, precum și numărul de secvențe de lungime maximă cu elemente divizibile cu 10.
+void sol17() {
+
+	int date[] = { 12,10,20,30,99,1000,1,20,500,600,5 };
+
+	int dim = 11;
+
+	secv10(date, dim);
+
+}
+
+//Se dă un vector format din n elemente, numere naturale nenule, şi un număr natural S. Determinaţi, dacă există o secvenţă de elemente din şir cu suma elementelor egală cu S.
+void sol18() {
+
+
+	int date[] = { 12,10,15,7,17,13,19,14 };
+
+	int dim = 8;
+
+	int S = 32;
+
+	SumaInSecv(date, dim, S);
+
+}
+
+//Se dă un şir format din n numere naturale nenule. Să se afle numărul secvenţelor din şir care au produsul elementelor egal cu 2k, unde k este un număr natural dat.
+void sol19() {
+
+	int date[] = { 7,4,2,4,5 };
+
+	int dim = 5;
+
+	int k = 2;
+
+	Memory006(date, dim, k);
+
+}
+
+//Se dă un șir cu n elemente, numere naturale. Determinați cea mai lungă secvență de elemente din șir cu proprietatea că oricare două valori consecutive se termină cu aceeași cifră.
+void sol20() {
+
+	int date[] = { 25,41,31,651,74,52,2,522,80,10 };
+
+	int dim = 10;
+
+
 
 }
